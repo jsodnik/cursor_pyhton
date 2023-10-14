e9afaf2c81306a07f9e58446dc2083264cdaeaa4
FE1\jakaso <jaka.sodnik@fe.uni-lj.si>
2023-10-14 17:05:37 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#####################
+# Welcome to Cursor #
+#####################
+def generate_2d_array(rows, cols):
+    return [[0 for _ in range(cols)] for _ in range(rows)]
+
+def main():
+    # Your code goes here
+    print("Jaka")
+    two_d_array = generate_2d_array(5, 5)
+    print(two_d_array)
+
+if __name__ == "__main__":
+    main()
